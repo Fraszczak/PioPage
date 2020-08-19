6d70c09d541777a4bc4a6291bcef98741d78cdb2
Piotr Frąszczak <Piotr.Fraszczak@hotmail.com>
2020-08-19 21:26:22 +0200

    Register/Login form on every active page or?

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PioPage.Areas.Identity.Data;
+using PioPage.Data;
+
+[assembly: HostingStartup(typeof(PioPage.Areas.Identity.IdentityHostingStartup))]
+namespace PioPage.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PioPageContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("PioPageContextConnection")));
+
+                services.AddDefaultIdentity<PioPageUser>(options =>
+                    {
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                        options.SignIn.RequireConfirmedAccount = false;
+                    })
+                    .AddEntityFrameworkStores<PioPageContext>();
+            });
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 using PioPage.Models;
 
 namespace PioPage.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -20,6 +23,7 @@ namespace PioPage.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Year = DateAndTime.Year(DateTime.Now);
             return View();
         }
 
@@ -28,6 +32,30 @@ namespace PioPage.Controllers
             return View();
         }
 
+        public IActionResult SingUp()
+        {
+            return View();
+        }
+
+        public IActionResult Author()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult Tech()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
